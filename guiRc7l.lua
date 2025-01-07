60617cce2c6306eb30df1a811224a6a8b65cc5b3
lolzerbypass99max <kainaishak@gmail.com>
2025-01-07 16:51:07 +0100

    Update guiRc7l.lua

--- guiRc7l.lua
+++ guiRc7l.lua
@@ -2,6 +2,8 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("badPs2max","BloodTheme")
 
 local Tab = Window:NewTab("Script")
+local Section = Tab:NewSection("Ro-Xploit")
+Section:NewLabel("Here")
 
 
 Section:NewButton("ro-xploit 6.0", "ButtonInfo", function()
