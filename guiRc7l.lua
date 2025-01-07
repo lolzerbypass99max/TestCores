fb5404d05a4503af171582167f7a9bdd58251c76
lolzerbypass99max <kainaishak@gmail.com>
2025-01-07 16:40:59 +0100

    Add files via upload
    Idk

---
+++ guiRc7l.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("badPs2max","BloodTheme")
+
+local Tab = Window:NewTab("Script")
+
+
+Section:NewButton("ro-xploit 6.0", "ButtonInfo", function()
+loadstring(game:GetObjects("rbxassetid://364364477")[1].Source)()
+end)
